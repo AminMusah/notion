4f111936da6c4e0b1ad9259ad943bf67e8bc94c0
aminmusah <ameenmusah77@gmail.com>
2025-04-27 03:14:26 +0100

    Add document editing

---
+++ components/editor.tsx
@@ -0,0 +1,45 @@
+"use client";
+import React, { useState } from "react";
+import "@blocknote/core/fonts/inter.css";
+import { BlockNoteView } from "@blocknote/mantine";
+import "@blocknote/mantine/style.css";
+import { useCreateBlockNote } from "@blocknote/react";
+import { Block, PartialBlock } from "@blocknote/core";
+import { useEdgeStore } from "@/lib/edgestore";
+
+type Props = {
+  onChange: (value: string) => void;
+  initialContent?: string;
+  editable?: boolean;
+};
+
+export default function Editor({ onChange, initialContent, editable }: Props) {
+  const { edgestore } = useEdgeStore();
+
+  const handleUpload = async (file: File) => {
+    const response = await edgestore.publicFiles.upload({
+      file,
+    });
+
+    return response.url;
+  };
+
+  const editor = useCreateBlockNote({
+    initialContent: initialContent
+      ? (JSON.parse(initialContent) as PartialBlock[])
+      : undefined,
+    uploadFile: handleUpload,
+  });
+
+  return (
+    <div>
+      <BlockNoteView
+        editor={editor}
+        onChange={() => {
+          // Saves the document JSON to state.
+          onChange(JSON.stringify(editor.document, null, 2));
+        }}
+      />
+    </div>
+  );
+}
